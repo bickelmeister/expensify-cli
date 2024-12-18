82638a2aaad901f4e1741482b90b5e391454480c
Elmar Bickel <35369684+bickelmeister@users.noreply.github.com>
2024-12-18 19:42:53 +0100

    add husky for git hooks

--- src/setup.ts
+++ src/setup.ts
@@ -1,33 +1,33 @@
-import path from 'path';
-import { config } from 'dotenv';
-import { existsSync, mkdirSync } from 'fs';
-import { writeJSONSync } from 'fs-extra/esm';
+import path from 'path'
+import { config } from 'dotenv'
+import { existsSync, mkdirSync } from 'fs'
+import { writeJSONSync } from 'fs-extra/esm'
 
-const ENV_FILE = path.join(process.cwd(), '.env');
+const ENV_FILE = path.join(process.cwd(), '.env')
 
 export const loadConfig = (): { transactionFile: string } => {
-  config({ path: ENV_FILE });
+  config({ path: ENV_FILE })
 
-  const transactionFile = process.env.TRANSACTION_FILE;
+  const transactionFile = process.env.TRANSACTION_FILE
 
   if (!transactionFile) {
     console.error(
-      'TRANSACTION_FILE in der Konfiguration nicht gefunden. Bitte Setup ausführen.'
-    );
-    process.exit(1);
+      'TRANSACTION_FILE in der Konfiguration nicht gefunden. Bitte Setup ausführen.',
+    )
+    process.exit(1)
   }
 
-  const transactionDir = path.dirname(transactionFile);
+  const transactionDir = path.dirname(transactionFile)
 
   if (!existsSync(transactionDir)) {
-    mkdirSync(transactionDir, { recursive: true });
-    console.log(`Verzeichnis erstellt: ${transactionDir}`);
-  }
+    mkdirSync(transactionDir, { recursive: true })
+    console.log(`Verzeichnis erstellt: ${transactionDir}`)
+  }    
 
   if (!existsSync(transactionFile)) {
-    writeJSONSync(transactionFile, []);
-    console.log(`Datei erstellt: ${transactionFile}`);
+    writeJSONSync(transactionFile, [])
+    console.log(`Datei erstellt: ${transactionFile}`)
   }
 
-  return { transactionFile };
-};
+  return { transactionFile }
+}
