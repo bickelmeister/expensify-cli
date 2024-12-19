d17cd0209d066565cb64b1c9892a45853cbae4e7
Elmar Bickel <35369684+bickelmeister@users.noreply.github.com>
2024-12-19 08:52:18 +0100

    fix error for no TRANSACTION_FILE

--- src/setup.ts
+++ src/setup.ts
@@ -3,20 +3,14 @@ import { config } from 'dotenv'
 import { existsSync, mkdirSync } from 'fs'
 import { writeJSONSync } from 'fs-extra/esm'
 
-const ENV_FILE = path.join(process.cwd(), '.env')
+const TRANSACTION_FILE = path.join(
+  process.cwd(),
+  'storage',
+  'transactions.json',
+)
 
 export const loadConfig = (): { transactionFile: string } => {
-  config({ path: ENV_FILE })
-
-  const transactionFile = process.env.TRANSACTION_FILE
-
-  if (!transactionFile) {
-    console.error(
-      'TRANSACTION_FILE in der Konfiguration nicht gefunden. Bitte Setup ausführen.',
-    )
-    process.exit(1)
-  }
-
+  const transactionFile = TRANSACTION_FILE
   const transactionDir = path.dirname(transactionFile)
 
   if (!existsSync(transactionDir)) {
